7d1e533d9a81ae580d98a3a48c25bec35e9c39fb
yssn10 <yassin.addala.pro@gmail.com>
2026-05-01 11:44:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,3 +36,7 @@ def chat():
         answer = "Erreur IA"
 
     return jsonify({"response": answer})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
